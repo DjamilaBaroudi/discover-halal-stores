d73dc74a8541ef76ee5c2d0238dc1223984af7cc
Djamila Baroudi <db@blksail.ai>
2022-11-10 10:13:44 +0100

    adds a new font from google fonts

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return <Html lang="en">
+            <Head>
+                <link
+                    rel="preload"
+                    href='/fonts/IBMPlexMono-Regular.ttf'
+                    as='font'
+                    crossOrigin='anonymous'>
+                </link>
+                <link
+                    rel="preload"
+                    href='/fonts/IBMPlexMono-SemiBold.ttf'
+                    as='font'
+                    crossOrigin='anonymous'>
+                </link> <link
+                    rel="preload"
+                    href='/fonts/IBMPlexMono-Bold.ttf'
+                    as='font'
+                    crossOrigin='anonymous'>
+                </link>
+            </Head>
+            <body>
+                <Main></Main>
+                <NextScript/>
+            </body>
+        </Html>
+    }
+}
+
+export default MyDocument
