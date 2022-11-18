d0cf0ee34fd3a408a81cd2d632153c385e3b0795
Djamila Baroudi <db@blksail.ai>
2022-11-18 13:56:07 +0100

    finds a store by id in airtable base

--- pages/api/createHalalStore.js
+++ pages/api/createHalalStore.js
@@ -3,3 +3,33 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
     .base(process.env.AIRTABLE_BASE_KEY);
 
 const table = base('halal-stores');
+
+console.log({ table });
+
+const createHalalStore = async (req, res) => {
+    // find a record
+    if (req.method === 'POST') {
+        try {
+            const findHalalStoreRecords = await table.select({
+                filterByFormula: `id="0"`
+            }).firstPage
+            if (findHalalStoreRecords.length !== 0) {
+                const records = findHalalStoreRecords.map(record => {
+                    return ({ ...record.fields });
+                })
+                res.json({ records });
+            }
+            else {
+                // create a record if not found 
+                res.json({ message: 'create a record' });
+            }
+        } catch (err) {
+            console.error('Error finding store', err);
+            res.status(500);
+            res.json({ message: 'Error finding store!', err });
+        };
+
+    }
+}
+
+export default createHalalStore;
