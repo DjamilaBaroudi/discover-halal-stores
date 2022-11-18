3fe75dac493f359d806efc93205ee3d18920817d
Djamila Baroudi <db@blksail.ai>
2022-11-18 14:34:25 +0100

    creates a new record (store) if it does'nt exists in airtable

--- pages/api/createHalalStore.js
+++ pages/api/createHalalStore.js
@@ -8,23 +8,50 @@ console.log({ table });
 
 const createHalalStore = async (req, res) => {
     // find a record
+    const { id, name, address, neighborhood, category, review, rating, image_url } = req.body;
     if (req.method === 'POST') {
         try {
             const findHalalStoreRecords = await table.select({
-                filterByFormula: `id="0"`
+                filterByFormula: `id=${id}`
             }).firstPage
             if (findHalalStoreRecords.length !== 0) {
                 const records = findHalalStoreRecords.map(record => {
                     return ({ ...record.fields });
                 })
-                res.json({ records });
+                res.json(records);
             }
             else {
                 // create a record if not found 
-                res.json({ message: 'create a record' });
+                if (id && name) {
+                    const createdRecords = await table.create([{
+                        fields: {
+                            id,
+                            name,
+                            address,
+                            neighborhood,
+                            category,
+                            review,
+                            rating,
+                            image_url
+                        }
+                    }]);
+                    const records = createdRecords.map(record => {
+                        return ({
+                            ...record.fields
+                        })
+                    })
+                    res.json(records)
+                }
+                else {
+                    //request contains bad syntax or can't be fulfilled
+                    res.status(400);
+                    res.json({ message: 'id or name is missing' });
+                }
+
             }
         } catch (err) {
             console.error('Error finding store', err);
+            //The server failed to fulfill an apparently valid request
             res.status(500);
             res.json({ message: 'Error finding store!', err });
         };
