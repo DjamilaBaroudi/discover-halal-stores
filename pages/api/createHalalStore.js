6f9a39e92900c0332b6f28cf9aa2c42f2b6bd020
Djamila Baroudi <db@blksail.ai>
2022-11-18 14:40:08 +0100

    checks if the id and name exists otherwise throw an error

--- pages/api/createHalalStore.js
+++ pages/api/createHalalStore.js
@@ -11,49 +11,55 @@ const createHalalStore = async (req, res) => {
     const { id, name, address, neighborhood, category, review, rating, image_url } = req.body;
     if (req.method === 'POST') {
         try {
-            const findHalalStoreRecords = await table.select({
-                filterByFormula: `id=${id}`
-            }).firstPage
-            if (findHalalStoreRecords.length !== 0) {
-                const records = findHalalStoreRecords.map(record => {
-                    return ({ ...record.fields });
-                })
-                res.json(records);
-            }
-            else {
-                // create a record if not found 
-                if (id && name) {
-                    const createdRecords = await table.create([{
-                        fields: {
-                            id,
-                            name,
-                            address,
-                            neighborhood,
-                            category,
-                            review,
-                            rating,
-                            image_url
-                        }
-                    }]);
-                    const records = createdRecords.map(record => {
-                        return ({
-                            ...record.fields
-                        })
+            if (id) {
+                const findHalalStoreRecords = await table.select({
+                    filterByFormula: `id=${id}`
+                }).firstPage
+                if (findHalalStoreRecords.length !== 0) {
+                    const records = findHalalStoreRecords.map(record => {
+                        return ({ ...record.fields });
                     })
-                    res.json(records)
+                    res.json(records);
                 }
                 else {
-                    //request contains bad syntax or can't be fulfilled
-                    res.status(400);
-                    res.json({ message: 'id or name is missing' });
-                }
+                    // create a record if not found 
+                    if (name) {
+                        const createdRecords = await table.create([{
+                            fields: {
+                                id,
+                                name,
+                                address,
+                                neighborhood,
+                                category,
+                                review,
+                                rating,
+                                image_url
+                            }
+                        }]);
+                        const records = createdRecords.map(record => {
+                            return ({
+                                ...record.fields
+                            })
+                        })
+                        res.json(records)
+                    }
+                    else {
+                        //request contains bad syntax or can't be fulfilled
+                        res.status(400);
+                        res.json({ message: 'name is missing' });
+                    }
 
+                }
+            } else {
+                //request contains bad syntax or can't be fulfilled
+                res.status(400);
+                res.json({ message: 'id is missing' });
             }
         } catch (err) {
-            console.error('Error finding store', err);
+            console.error('Error creating or finding a store', err);
             //The server failed to fulfill an apparently valid request
             res.status(500);
-            res.json({ message: 'Error finding store!', err });
+            res.json({ message: 'Error creating or finding a store!', err });
         };
 
     }
