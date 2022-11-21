fe234f84ee3336931cf3faf7c046b330ef0d03ce
Djamila Baroudi <db@blksail.ai>
2022-11-21 12:38:37 +0100

    gives the rating some lables

--- components/rating.js
+++ components/rating.js
@@ -5,6 +5,7 @@ import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { styled } from '@mui/material/styles';
 import styles from './rating.module.css';
+import  Box  from '@mui/material/Box';
 
 const StyledRating = styled(Rating)({
     '& .MuiRating-icon': {
@@ -19,19 +20,37 @@ const StyledRating = styled(Rating)({
         fontSize: '2rem'
     },
 });
-
+const labels = {
+    0:'No rating yet',
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor+',
+    2.5: 'Ok',
+    3: 'Ok+',
+    3.5: 'Good',
+    4: 'Good+',
+    4.5: 'Excellent',
+    5: 'Excellent+',
+};
 const StarRating = (props) => {
     return (
         <div className={styles.container}>
             <StyledRating
                 name="customized-color"
                 defaultValue={0}
-                getLabelText={(value) => `${value} Star${value !== 1 ? 's' : ''}`}
+                getLabelText={props.getLabelText}
+                value={props.value}
                 precision={0.5}
                 icon={<StarIcon fontSize="inherit" />}
+                onChange={props.onChange}
                 emptyIcon={<StarBorderIcon fontSize="inherit" />}
             />
+            {props.value !== null && (
+                <Box sx={{ ml: 2 }}>{labels[props.value]}</Box>
+            )}
         </div>
+
     )
 }
 
